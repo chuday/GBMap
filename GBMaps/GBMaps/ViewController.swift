d89ca1043cd0205b2ffb96eb7a9f829801056b04
chuday <mikhail_chudaew505@mail.ru>
2022-06-28 09:16:15 +0500

    Add proj

---
+++ GBMaps/GBMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBMaps
+//
+//  Created by Mikhail Chudaev on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
